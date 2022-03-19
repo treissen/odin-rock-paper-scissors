2109e9bbde05ec4f02a44c764a10224369f1113d
treissen <timreissenart@gmail.com>
2022-03-18 22:05:52 -0600

    game working in console

--- script.js
+++ script.js
@@ -5,23 +5,18 @@ let userScore = 0
 let roundCount = 0
 let winner = ''
 let userSelection
-
 const choices = ['rock', 'paper', 'scissors']
 
 const handleClick = (e) => {
     userSelection = e.target.value
-    // console.log(userSelection)
-    computerPlay()
-    // console.log(compSelection)
-    playRound(userSelection, compSelection);
+    game()
     
 }
 
 for (var i = 0 ; i < choices.length; i++) {
    addEventListener('click' ,handleClick); 
- }
+}
 
-// computer choice
 
 function computerPlay() {
     let compArray = ['rock', 'paper', 'scissors'];
@@ -29,38 +24,30 @@ function computerPlay() {
     return compSelection
 }
 
-// user input
-
-// function userInput() {
-//     userPrompt = prompt('rock, paper, or scissors?');
-//     userSelection = userPrompt.toLowerCase();
-//     return userSelection
-// }
-// change this to buttons
-
-
-
 function playRound(userSelection, compSelection) {
-      
     switch (userSelection + compSelection) {
         case 'scissorspaper':
         case 'rockscissors':
         case 'paperrock':
             result =  'User wins this round'
             ++userScore
+            ++roundCount
             break
         case 'paperscissors':
         case 'scissorsrock':
         case 'rockpaper':
             result =  'computer wins this round';
             ++compScore
+            ++roundCount
             break
         case 'scissorsscissors':
         case 'rockrock':
         case 'paperpaper':
             result = 'this round is a tie'
+            ++roundCount
             break
   }
+//   console.log(result)
   return result
 }
 
@@ -79,23 +66,19 @@ function gameWinner() {     //this will run after the game() loops 5 times and w
 
 //  game() needs to loop through  playRound(userSelection, compSelection) 5 times
 function game(){
-    for(let roundCount = 1; roundCount < 6; roundCount++) {   // start roundcount at 1 instead of zero so it logs what round just played
-        userSelelction = userInput();                         // call user input function set to userSelection
-        console.log('you chose: ' + userSelection);           // log user selection
-        compSelection = computerPlay();                       // call computer play function set to Computer Play
-        console.log('Computer selected ' + compSelection);    // log computer selection
-        playRound(userSelection,compSelection);               // run playround function
-        console.log('result: ' + result)                      //log result of round
-        console.log('userScore: ' + userScore)                // log user score
-        console.log('compScore: ' + compScore)                // log compscore
-        console.log('that was round ' + roundCount)           // log what round was just played
-    }
-
-    // after loops through 5 times runs function to log who won the match
-    gameWinner()
-    console.log('the winner of the game is......' + winner) 
+    
+    console.log('you chose: ' + userSelection);           
+    compSelection = computerPlay();                       
+    console.log('Computer selected ' + compSelection);    
+    playRound(userSelection,compSelection);               
+    console.log('result: ' + result)                      
+    console.log('userScore: ' + userScore)                
+    console.log('compScore: ' + compScore)                
+    console.log('that was round ' + roundCount)           
+    if(roundCount === 5){
+        gameWinner();
+        console.log('the winner of the game is......' + winner)
+    }    
 }
-// calling game() on click event
 
-
-// test on github desktop
+// for(let roundCount = 1; roundCount < 6; roundCount++){}
